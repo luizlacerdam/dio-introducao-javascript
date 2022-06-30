e7d00c018b571a13d0a387bce28483be611328ee
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-06-30 10:13:51 -0300

    alteracoes com addEventListener

--- introducao-ao-javascript/contador/assets/scripts.js
+++ introducao-ao-javascript/contador/assets/scripts.js
@@ -2,9 +2,11 @@ let count = 0;
 
 const CURRENT_NUMBER = document.getElementById('currentNumber');
 
-function increment() {
+function increment() {		
 	count++;
 	CURRENT_NUMBER.innerHTML = count;
+
+	
 }
 
 function decrement() {
@@ -12,6 +14,36 @@ function decrement() {
 	CURRENT_NUMBER.innerHTML = count;
 }
 
-function test() {
-	kdowkdpo;
-}
+
+let decre = document.getElementById('decre');
+let incre = document.getElementById('incre');
+
+decre.addEventListener('click', function(event){
+	if(count < 0) {
+		CURRENT_NUMBER.style.color = 'Red'
+	} else {
+		CURRENT_NUMBER.style.color = 'Black'
+	}
+	if(count <= -10) {
+		decre.disabled = true;
+		
+	} else {
+		incre.disabled = false;
+		decre.disabled = false;
+	}
+} )
+
+incre.addEventListener('click', function(event){
+	if(count < 0) {
+		CURRENT_NUMBER.style.color = 'Red'
+	} else {
+		CURRENT_NUMBER.style.color = 'Black'
+	}
+	if(count >= 10) {
+		incre.disabled = true;
+		
+	} else {
+		incre.disabled = false;
+		decre.disabled = false;
+	}
+} )
