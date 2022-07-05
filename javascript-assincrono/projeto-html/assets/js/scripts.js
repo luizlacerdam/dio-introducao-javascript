1f4029407da4ae21c28f35a56d27e9b4d6210576
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-05 14:09:18 -0300

    erasing and upping

--- javascript-assincrono/projeto-html/assets/js/scripts.js
+++ javascript-assincrono/projeto-html/assets/js/scripts.js
@@ -1,2 +1,25 @@
 const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const catBtn = document.getElementById('change-cat');
+
+
+	const cats = async () => {
+		try {
+			const data = await fetch(BASE_URL);
+			const json = await data.json();
+			return json.weburl;
+		} catch (e) {
+			console.log(e.message);
+		}
+	
+	}
+	
+	const loadImg = async () => {
+		const catImg = document.getElementById('cat');
+		catImg.src = await cats();
+	}
+
+	catBtn.addEventListener('click', loadImg);
+
+	loadImg();
+
 
